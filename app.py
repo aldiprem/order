3dc1bc996389a69921b9592c41982f76eae262c1
aldiprem <premiumaldi053@gmail.com>
2026-02-22 04:38:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -358,8 +358,8 @@ if __name__ == '__main__':
     print("="*50)
     print("🚀 Starting Flask Server...")
     print("📁 Database: telegram_users.db")
-    print("🔗 URL: http://localhost:5000")
+    print("🔗 URL: http://localhost:5005")
     print("="*50)
     
     # Jalankan dengan debug mode
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5005)
