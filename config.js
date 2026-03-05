f7dc1774863e35a762436da55ab74f5f7921f9d2
aldiprem <premiumaldi053@gmail.com>
2026-03-05 22:46:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js - Configuration file
 window.CONFIG = {
   APP_NAME: 'INDOTAG MARKET',
-  API_BASE_URL: 'https://backing-professor-song-burn.trycloudflare.com',
+  API_BASE_URL: 'https://thesaurus-characters-lesser-boutique.trycloudflare.com',
   PULL_TO_REFRESH_THRESHOLD: 80,
   VERSION: '1.0.0'
 };
