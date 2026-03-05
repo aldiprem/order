64ab4f3e4d8364334a3a1c36903ced1ee074a0d1
aldiprem <premiumaldi053@gmail.com>
2026-03-05 23:03:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js - Configuration file
 window.CONFIG = {
-    APP_NAME: 'INDOTAG MARKET',
-    API_BASE_URL: 'https://thesaurus-characters-lesser-boutique.trycloudflare.com', // URL tunnel
-    PULL_TO_REFRESH_THRESHOLD: 80,
-    VERSION: '1.0.0'
+  APP_NAME: 'INDOTAG MARKET',
+  API_BASE_URL: 'https://steal-debate-semester-feature.trycloudflare.com',
+  PULL_TO_REFRESH_THRESHOLD: 80,
+  VERSION: '1.0.0'
 };
